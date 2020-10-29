e1f4dab4518e7d53c3e8ba662e4f7b0f4efa7b0b
Willian <SantaAna>
2020-10-29 20:37:59 +0000

    Start Spring Boot Application

---
+++ src/main/java/pt/iade/HelloWordSpringBoot/HelloWordSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.HelloWordSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordSpringBootApplication.class, args);
+	}
+
+}
